bcc99b36b7e95cc9e5ff450ecc268e5f137dc945
La <90055383+paulagrata@users.noreply.github.com>
2023-08-03 16:12:41 -0500

    booptoot.

---
+++ ufo.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "ufo_functions.hpp"
+
+int main() {
+  greet();
+
+  std::string codeword = "bumfuzzle";
+  std::string answer = "__________";
+  int misses = 0;
+  std::vector<char> incorrect;
+  bool guess = false;
+  char letter;
+
+  while (answer != codeword && misses < 7) {
+    display_misses(misses);
+    display_status(incorrect, answer);
+
+    std::cout << "\n\nenter letter guess: ";
+    std::cin >> letter;
+
+    for (int i = 0; i < codeword.length(); i++) {
+      if (letter == codeword[i]) {
+        answer[i] = letter;
+        guess = true;
+      }
+    }
+
+    if (guess) {
+      std::cout << "\heckyes!\n";
+    } else {
+      std::cout << "\nbetter luck next time! the tractor beam pulls the person in further. muahahahaha.\n";
+      incorrect.push_back(letter);
+      misses++;
+    }
+    guess = false;
+  }
+
+  end_game(answer, codeword);
+}
