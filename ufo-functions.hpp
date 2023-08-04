088dd2e2806a862d1413f21a027e262aaa7bac10
La <90055383+paulagrata@users.noreply.github.com>
2023-08-04 15:25:38 -0500

    boop.

--- ufo-functions.hpp
+++ ufo-functions.hpp
@@ -1,6 +1,7 @@
 #include <vector>
 
 void greet();
+std::string getWord();
 void display_status(std::vector<char> incorrect, std::string answer);
 void display_misses(int misses);
 void end_game(std::string answer, std::string codeword);
